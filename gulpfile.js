1710663a7a1c9d12874f74f61e701fde8e809c9f
Tam Pham <tampham47@live.com>
2016-08-22 07:32:19 +0700

    add polyfill

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-var gulp = require('gulp'),
-  $ = require('gulp-load-plugins')();
+/**
+ *
+ */
+
+require('es6-promise').polyfill();
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
 gulp.task('css', function() {
   return gulp.src('app/styles/main.scss')
