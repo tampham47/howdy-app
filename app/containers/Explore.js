90943bace7aa16b873f307ef4777dc93e2d7e1bf
Tam Pham <tampham47@live.com>
2016-08-25 19:52:48 +0700

    add explore page

--- app/containers/Explore.js
+++ app/containers/Explore.js
@@ -29,10 +29,97 @@ class Explore extends Component {
         <LeftMenu/>
 
         <main className="main-area">
-          <HeaderBar title='Explore' />
+          <HeaderBar title='Explore chanels' />
 
           <div className="main-content main-content--expand">
-            <p>main</p>
+            <div className="row">
+              <div className="columns four">
+                <div className="room-item">
+                  <div className="room-item__head">
+                    <div className="room-item__head__icon">
+                      <img src="http://place-hoff.com/50/50" alt=""/>
+                    </div>
+                    <h6 className="room-item__head__title">Goingsunny</h6>
+                  </div>
+                  <div className="room-item__body">
+                    <p>Bài thơ đầu anh viết tặng em, là bài thơ kể về đôi dép, khi nỗi nhớ trong...</p>
+                    <button>view</button>
+                  </div>
+                </div>
+              </div>
+              <div className="columns four">
+                <div className="room-item">
+                  <div className="room-item__head">
+                    <div className="room-item__head__icon">
+                      <img src="http://place-hoff.com/45/45" alt=""/>
+                    </div>
+                    <h6 className="room-item__head__title">English Town</h6>
+                  </div>
+                  <div className="room-item__body">
+                    <p>Bài thơ đầu anh viết tặng em, là bài thơ kể về đôi dép, khi nỗi nhớ trong...</p>
+                    <button>view</button>
+                  </div>
+                </div>
+              </div>
+              <div className="columns four">
+                <div className="room-item">
+                  <div className="room-item__head">
+                    <div className="room-item__head__icon">
+                      <img src="http://place-hoff.com/55/55" alt=""/>
+                    </div>
+                    <h6 className="room-item__head__title">Tiếng anh thật dễ</h6>
+                  </div>
+                  <div className="room-item__body">
+                    <p>Bài thơ đầu anh viết tặng em, là bài thơ kể về đôi dép, khi nỗi nhớ trong...</p>
+                    <button>view</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="columns four">
+                <div className="room-item">
+                  <div className="room-item__head">
+                    <div className="room-item__head__icon">
+                      <img src="http://place-hoff.com/41/41" alt=""/>
+                    </div>
+                    <h6 className="room-item__head__title">Tiếng anh cho người lớn</h6>
+                  </div>
+                  <div className="room-item__body">
+                    <p>Bài thơ đầu anh viết tặng em, là bài thơ kể về đôi dép, khi nỗi nhớ trong...</p>
+                    <button>view</button>
+                  </div>
+                </div>
+              </div>
+              <div className="columns four">
+                <div className="room-item">
+                  <div className="room-item__head">
+                    <div className="room-item__head__icon">
+                      <img src="http://place-hoff.com/52/52" alt=""/>
+                    </div>
+                    <h6 className="room-item__head__title">Topica Native</h6>
+                  </div>
+                  <div className="room-item__body">
+                    <p>Bài thơ đầu anh viết tặng em, là bài thơ kể về đôi dép, khi nỗi nhớ trong...</p>
+                    <button>view</button>
+                  </div>
+                </div>
+              </div>
+              <div className="columns four">
+                <div className="room-item">
+                  <div className="room-item__head">
+                    <div className="room-item__head__icon">
+                      <img src="http://place-hoff.com/53/53" alt=""/>
+                    </div>
+                    <h6 className="room-item__head__title">ABA English</h6>
+                  </div>
+                  <div className="room-item__body">
+                    <p>Bài thơ đầu anh viết tặng em, là bài thơ kể về đôi dép, khi nỗi nhớ trong...</p>
+                    <button>view</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div>
