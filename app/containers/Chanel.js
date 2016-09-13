e0332634acc7be3485da434d320d299521fddba2
Tam Pham <tampham47@live.com>
2016-09-13 21:23:28 +0700

    update layout

--- app/containers/Chanel.js
+++ app/containers/Chanel.js
@@ -206,31 +206,34 @@ class Chanel extends Component {
         <LeftMenu chanelList={chanelData.chanelList} />
 
         <main className="main-area">
-          <HeaderBar title={channelDetail.name} datacontext={channelDetail} />
-
           <div className="main-content">
-            <div id='content-scroller' className="main-content__scroller">
-              <div className="message-list">
-                {messageList.map(function(item, index) {
-                  item.authUser = item.authUser || {};
-                  return (
-                    <div className="message-item" key={index}>
-                      <div className="message-item__icon">
-                        <img src={item.authUser.avatar} alt=""/>
-                      </div>
-                      <div className="message-item__wrapper">
-                        <div className="message-item__title">
-                          <span className="message-item__title__name">{item.authUser.displayName}</span>
-                          <small className="message-item__title__username">{`@${item.authUser.displayName}`}</small>
-                          <small className="message-item__title__time">Jul 03 19:46</small>
+            <HeaderBar title={channelDetail.name} datacontext={channelDetail} />
+
+            <div className="main-content__wrapper">
+              <div id='content-scroller' className="main-content__scroller">
+                <div className="message-list">
+                  {messageList.map(function(item, index) {
+                    item.authUser = item.authUser || {};
+                    return (
+                      <div className="message-item" key={index}>
+                        <div className="message-item__icon">
+                          <img src={item.authUser.avatar} alt=""/>
                         </div>
-                        <MessageItem datacontext={item} />
+                        <div className="message-item__wrapper">
+                          <div className="message-item__title">
+                            <span className="message-item__title__name">{item.authUser.displayName}</span>
+                            <small className="message-item__title__username">{`@${item.authUser.displayName}`}</small>
+                            <small className="message-item__title__time">Jul 03 19:46</small>
+                          </div>
+                          <MessageItem datacontext={item} />
+                        </div>
                       </div>
-                    </div>
-                  );
-                }.bind(this))}
+                    );
+                  }.bind(this))}
+                </div>
               </div>
             </div>
+
             <div className="input-area">
               <div className="input-area__wrapper">
                 <div className="input-area__icon">
