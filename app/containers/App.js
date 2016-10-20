c26d7115e0c5ae19125716806486de782a471f85
Tam Pham <tampham47@live.com>
2016-10-20 07:39:57 +0700

    update http command

--- app/containers/App.js
+++ app/containers/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   componentDidMount() {
 
-    if (window.location.protocol === 'http://') {
+    if (window.location.protocol === 'http:') {
       client.on('connect', function() {
         client.subscribe('goingsunny');
       }.bind(this));
