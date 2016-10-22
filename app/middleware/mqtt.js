11a650966f4c150ee33a90ed41897786c982162c
Tam Pham <tampham47@live.com>
2016-10-22 08:02:26 +0700

    using wss protocol

--- app/middleware/mqtt.js
+++ app/middleware/mqtt.js
@@ -7,9 +7,9 @@ import mqtt from 'mqtt';
 
 var client;
 if (typeof window !== 'undefined' && window.location.protocol === 'http:') {
-  var client = mqtt.connect('ws://goingsunny.com:5551');
+  var client = mqtt.connect('ws://broker.goingsunny.com');
 } else {
-  // var client = mqtt.connect('wss://goingsunny.com:5550');
+  var client = mqtt.connect('wss://broker.goingsunny.com');
 }
 
 export default client;
