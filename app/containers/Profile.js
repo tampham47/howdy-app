03908b7cbf775eb8853403dfa03b445940c63482
Tam Pham <tampham47@live.com>
2017-03-05 21:16:24 +0700

    add wave at the bottom

--- app/containers/Profile.js
+++ app/containers/Profile.js
@@ -64,55 +64,63 @@ class Profile extends Component {
     let channelList = this.props.channelList;
 
     return (
-      <div className="main-content">
-        <ChannelHeader title={'My Profile'} icon={'fa-chevron-left'} />
-        <div className="columns eight offset-by-two">
+      <div className="wave-wrapper-02">
+        <main className="main-area">
+          <ChannelHeader title={'My Profile'} icon={'fa-chevron-left'} />
           <div className="main-content main-content--expand">
-            <form className="add-room-form" onSubmit={()=>false}>
+            <div className="columns eight offset-by-two">
+              <form className="add-room-form" onSubmit={()=>false}>
 
-              <label htmlFor="displayName">Full Name</label>
-              <input className="u-full-width" type="text" name="displayName"
-                placeholder="" id="displayName"
-                value={this.state.form.displayName}
-                onChange={this.handleChanged.bind(this, 'displayName')} />
+                <label htmlFor="displayName">Full Name</label>
+                <input className="u-full-width" type="text" name="displayName"
+                  placeholder="" id="displayName"
+                  value={this.state.form.displayName}
+                  onChange={this.handleChanged.bind(this, 'displayName')} />
 
-              <label htmlFor="email">Email</label>
-              <input className="u-full-width" type="text" name="email"
-                placeholder="optional" id="email"
-                value={this.state.form.email}
-                onChange={this.handleChanged.bind(this, 'email')} />
+                <label htmlFor="email">Email</label>
+                <input className="u-full-width" type="text" name="email"
+                  placeholder="optional" id="email"
+                  value={this.state.form.email}
+                  onChange={this.handleChanged.bind(this, 'email')} />
 
-              <label htmlFor="username">Username</label>
-              <small>That helps other people mention you in a message</small>
-              <input className="u-full-width" type="text" name="username"
-                placeholder="optional" id="username"
-                value={this.state.form.username}
-                onChange={this.handleChanged.bind(this, 'username')} />
+                <label htmlFor="username">Username</label>
+                <small>That helps other people mention you in a message</small>
+                <input className="u-full-width" type="text" name="username"
+                  placeholder="optional" id="username"
+                  value={this.state.form.username}
+                  onChange={this.handleChanged.bind(this, 'username')} />
 
-              <label htmlFor="appearinLink">Appearin Profile</label>
-              <small>People can talk directly with you on appearin when you online from goingsunny</small>
-              <input className="u-full-width" type="url" name="appearinLink"
-                placeholder="optional" id="appearinLink"
-                value={this.state.form.appearinLink}
-                onChange={this.handleChanged.bind(this, 'appearinLink')} />
+                <label htmlFor="appearinLink">Appearin Profile</label>
+                <small>People can talk directly with you on appearin when you online from goingsunny</small>
+                <input className="u-full-width" type="url" name="appearinLink"
+                  placeholder="optional" id="appearinLink"
+                  value={this.state.form.appearinLink}
+                  onChange={this.handleChanged.bind(this, 'appearinLink')} />
 
-              <label htmlFor="bio">Bio</label>
-              <textarea className="u-full-width"
-                placeholder="..." name="bio" id="bio"
-                value={this.state.form.bio}
-                onChange={this.handleChanged.bind(this, 'bio')} >
-              </textarea>
+                <label htmlFor="bio">Bio</label>
+                <textarea className="u-full-width"
+                  placeholder="..." name="bio" id="bio"
+                  value={this.state.form.bio}
+                  onChange={this.handleChanged.bind(this, 'bio')} >
+                </textarea>
 
-              <div className="add-room-form__controls">
-                <input className="button button-primary" type="button" value="Submit"
-                  onClick={this.handleSubmitButton.bind(this)}/>
-                {/*<input className="button" type="button" value="Cancel"
-                  onClick={this.handleCancelButton.bind(this)} />*/}
-              </div>
-            </form>
+                <div className="add-room-form__controls">
+                  <input className="button button-primary" type="button" value="Submit"
+                    onClick={this.handleSubmitButton.bind(this)}/>
+                  {/*<input className="button" type="button" value="Cancel"
+                    onClick={this.handleCancelButton.bind(this)} />*/}
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
+
+        </main>
+
+        <footer>
+          <div className="wave"></div>
+        </footer>
       </div>
+
     )
   }
 }
